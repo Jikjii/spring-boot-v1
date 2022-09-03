c2549be0203d40872b841db31e50973979566bea
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2022-09-03 13:12:11 -0400

    feat: misc updates

---
+++ src/main/java/com/jikjii/Springboot/v1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.jikjii.Springboot.v1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Welcome to Jikjii";
+    }
+}
